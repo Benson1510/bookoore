ab6f80c1ce78d9af560ae7fed5e7b225d15226ec
Xydrion_AI <davidpetiot35@gmail.com>
2025-05-12 20:55:48 +0200

    Footer update

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -1,9 +1,92 @@
 
 
 function Footer() {
+    //const Logo = "";
     return (
-        <div class="container">
-            column
-        </div>
+        <footer className="bg-secondary text-white ">
+            <div className="container p-4">
+                <div className="row">
+                    <div className="col-lg-5 col-md-3 mb-4 mb-md-0 test2">
+                        <div className="row test1">
+                            <div className="col-2 test2">
+                                <img src="" alt="Logo de Bookeo" className='' />
+                            </div>
+                            <div className="col-5 test2">
+                                <h2>Bookeo</h2>
+                                <p>Book Store Website</p>
+                            </div>
+                            <p className="">
+                                Bookoe is a Book Store Website lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            </p>
+                            <h4>
+                                Follow Us
+                            </h4>
+                        </div>
+                    </div>
+
+                    <div className="col-lg-2 col-md-2 mb-4 mb-md-0 test2">
+                        <h5>Books Categories</h5>
+                        <div className="row test1">
+                            <div className="col-6 test1">
+                                <ul className="list-unstyled my-2">
+                                    <li className="my-3"><a href="#!" className="text-white">Action</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Advanture</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Comedy</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Crime</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Drama</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Fantasy</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Horror</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-6">
+                                <ul className="list-unstyled my-2">
+                                    <li className="my-3"><a href="#!" className="text-white">Law</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Mystery</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Professional</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">Romance</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">TV Series</a></li>
+                                    <li className="my-3"><a href="#!" className="text-white">View more ></a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-lg-2 col-md-2 mb-4 mb-md-0">
+                        <h5 className="mb-0">Quick Links</h5>
+                        <ul className="list-unstyled my-2">
+                            <li className="my-3"><a href="#!" className="text-white">About us</a></li>
+                            <li className="my-3"><a href="#!" className="text-white">Contact us</a></li>
+                            <li className="my-3"><a href="#!" className="text-white">Products</a></li>
+                            <li className="my-3"><a href="#!" className="text-white">Login</a></li>
+                            <li className="my-3"><a href="#!" className="text-white">Sign Up</a></li>
+                            <li className="my-3"><a href="#!" className="text-white">FAQ</a></li>
+                            <li className="my-3"><a href="#!" className="text-white">Shipment</a></li>
+                        </ul>
+                    </div>
+                    <div className="col-lg-2 col-md-2 mb-4 mb-md-0 test2">
+                        <div className="row test1">
+                            <div className="col-2 test2">
+                                <img src="" alt="Logo de Bookeo" className='' />
+                            </div>
+                            <div className="col-5 test2">
+                                <h2>Bookeo</h2>
+                                <p>Book Store Website</p>
+                            </div>
+                            <p>
+                                Bookoe is a Book Store Website lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            </p>
+                            <h4>
+                                Follow Us
+                            </h4>
+                        </div>
+                    </div>
+                    <div className="p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+                        Bookoe Book Store Website - © 2020 All Rights Reserved
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
+
+export default Footer;
